3d10a2cca24313dd4e00cda6e5c30c1d90becf8b
Derek Nunes <dereknunes28@gmail.com>
2022-05-12 23:29:42 -0500

    add listContent.js

---
+++ src/listContent.js
@@ -0,0 +1,16 @@
+// add list and task modules and page controllers
+
+// addList module
+const addList = () => {
+// create list name factory
+
+}
+
+
+// addTask module
+const addTask = () => {
+// create task info factory
+
+}
+
+// DOM logic
