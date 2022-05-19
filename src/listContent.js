17a552838174dc92883edab86bf18f6f62232ae8
Derek Nunes <dereknunes28@gmail.com>
2022-05-18 23:57:40 -0500

    create cards in js in addNewTask function

--- src/listContent.js
+++ src/listContent.js
@@ -1,5 +1,9 @@
 // add list and task modules and page controllers
 
+import deleteIcon from './assets/svgs/deleteIcon.svg';
+import editIcon from './assets/svgs/editIcon.svg'; 
+
+
 // addList module
 const addList = () => {
     
@@ -121,13 +125,80 @@ function submitNewTask() {
     const taskNotes = document.querySelector('#atm-notes');
     const task = new newTask(taskTitle.value, taskDescription.value, taskDueDate.value, taskNotes.value, 'no');
 
-    // create and append html card
+    // create and append html card elements
     const taskArea = document.querySelector('.list-tasks');
-    const element = document.createElement('div');
-    element.textContent = `${task.title}${task.description}${task.dueDate}${task.notes}${task.isComplete}`;
-    taskArea.append(element);
+    const card = newDiv('card','');
+    const cardContent = newDiv('card-content','');
+
+    const cardTaskTitle = newDiv('card-title','');
+    cardTaskTitle.classList.add('card-sectionLabel');
+    cardTaskTitle.textContent = task.title;
+
+    const cardTaskDescription = newDiv('card-description','');
+    const descriptionSpan = document.createElement('span');
+    descriptionSpan.classList.add('card-sectionLabel');
+    descriptionSpan.textContent = 'Description: ';
+    const descriptionText = document.createElement('span');
+    descriptionText.textContent = task.description
+    cardTaskDescription.append(descriptionSpan, descriptionText);
+
+    const cardTaskDueDate = newDiv('card-dueDate','');
+    const dueDateSpan = document.createElement('span');
+    dueDateSpan.classList.add('card-sectionLabel');
+    dueDateSpan.textContent = 'Due Date: ';
+    const dueDateText = document.createElement('span');
+    dueDateText.textContent = task.dueDate;
+    cardTaskDueDate.append(dueDateSpan,dueDateText);
+
+    const cardTaskNotes = newDiv('card-notes','');
+    const notesSpan = document.createElement('span');
+    notesSpan.classList.add('card-sectionLabel');
+    notesSpan.textContent = 'Notes: ';
+    const notesText = document.createElement('span');
+    notesText.textContent = task.notes;
+    cardTaskNotes.append(notesSpan,notesText);
+
+    const cardTaskCheckbox = newDiv('checkbox','');
+    const checkboxSpan = document.createElement('span');
+    checkboxSpan.classList.add('card-sectionLabel');
+    checkboxSpan.textContent = 'Complete?  ';
+    const checkboxInput = document.createElement('input');
+    checkboxInput.classList.add('task-complete');
+    checkboxInput.setAttribute('type','checkbox');
+    cardTaskCheckbox.append(checkboxSpan, checkboxInput);
+
+    const deleteIconSVG = new Image();
+    deleteIconSVG.src = deleteIcon;
+    deleteIconSVG.classList.add('card-button');
+    const editIconSVG = new Image();
+    editIconSVG.src = editIcon;
+    editIconSVG.classList.add('card-button');
+    const cardButtons = newDiv('card-deleteOrEdit','');
+    const editButton = document.createElement('button');
+    editButton.classList.add('card-edit');
+    editButton.append(editIconSVG);
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('card-delete');
+    deleteButton.append(deleteIconSVG);
+    cardButtons.append(editButton,deleteButton);
+
+    cardContent.append(cardTaskTitle, cardTaskDescription, cardTaskDueDate, cardTaskNotes, cardTaskCheckbox, cardButtons);
+    card.appendChild(cardContent);
+
+    taskArea.append(card);
 
     // clear add task modal
     taskTitle.value = taskDescription.value = taskDueDate.value = taskNotes.value = '';
     modalOpenOrClose('#addTaskModal','close');
+}
+
+function newDiv(classname, idname) {
+    const element = document.createElement('div');
+    if (classname !== '') {
+        element.classList.add(`${classname}`);    
+    }
+    if (idname !== '') {
+        element.setAttribute('id',`${idname}`);    
+    }
+    return element;
 }
